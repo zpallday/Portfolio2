da2b0db5573b2ff2b43cd4fb44a73d16eebcc408
Zach <zpman95@hotmail.com>
2020-04-07 11:36:45 -0500

    added changes

--- portfolio/src/pages/features.js
+++ portfolio/src/pages/features.js
@@ -10,7 +10,7 @@ export default function Features() {
           
               <h4 className="features-topic">
               <div>
-              <img src="https://i.ibb.co/tLsLJfj/react-icon.png"  width="22%" alt="react-icon" border="0"></img>
+              <img className="icons" src="https://i.ibb.co/tLsLJfj/react-icon.png"  width="20%" alt="react-icon" border="0"></img>
               </div>
                 React
               </h4>
@@ -21,7 +21,7 @@ export default function Features() {
            
               <h4 className="features-topic">
               <div>
-              <img src="https://i.ibb.co/mv2zcLr/download-4.png" width="20%" alt="react-icon" border="0"></img>
+              <img className="icons" src="https://i.ibb.co/mv2zcLr/download-4.png" width="20%" alt="react-icon" border="0"></img>
               </div>
                 Node.js
               </h4>
@@ -32,7 +32,7 @@ export default function Features() {
         
               <h4 className="features-topic">
               <div>
-              <img src="https://i.ibb.co/7nsBgcm/download-6.png" width="20%" alt="react-icon" border="0"></img>
+              <img className="icons" src="https://i.ibb.co/7nsBgcm/download-6.png" width="20%" alt="react-icon" border="0"></img>
               </div>
                 CSS
               </h4>
@@ -42,35 +42,35 @@ export default function Features() {
       </div>
     </div>
     <div className="features">
-      <div className="top-features">
+      <div className="">
         <div className="features-align">
           <div className="single-special">
           
-              <h4 className="features-topic">
+              <h4 className="features-topics">
               <div>
-              <img src="https://i.ibb.co/tLsLJfj/react-icon.png"  width="22%" alt="react-icon" border="0"></img>
+              <img className="icons"  src="https://i.ibb.co/D897RXC/iconfinder-267-Python-logo-4375050.png" width="20%" alt="react-icon" border="0"></img>
               </div>
-                React
+                Python
               </h4>
         
           </div>
 
-          <div className="single-special">
+          <div className="">
            
               <h4 className="features-topic">
               <div>
-              <img src="https://i.ibb.co/89NXxSg/js.png" width="22%" alt="react-icon" border="0"></img>
+              <img className="icons" src="https://i.ibb.co/89NXxSg/js.png" width="20%" alt="react-icon" border="0"></img>
               </div>
-                Node.js
+                Javascript
               </h4>
      
           </div>
 
           <div className="single-special">
         
-              <h4 className="features-topic">
+              <h4 className="features-topicsss">
               <div>
-              <img src="https://i.ibb.co/NC6xMn9/download-3.png"  width="20%" alt="react-icon" border="0"></img>
+              <img className="icons" src="https://i.ibb.co/NC6xMn9/download-3.png"  width="20%" alt="react-icon" border="0"></img>
               </div>
                 CSS
               </h4>
